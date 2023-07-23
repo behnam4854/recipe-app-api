613a95bc1d2313f0792502b6da28a44124b72e35
ibehnam <behnam4854@gmail.com>
2023-07-23 20:06:35 +0330

    added create tag test and functionality for tag

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -59,4 +59,18 @@ class PrivateTagsApiTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
-        
+    
+    def test_create_tag_successful(self):
+        """test for creating a tag"""
+        payload = {'name' : 'testtag'}
+        self.client.post(TAGS_URL,payload)
+        res = Tag.objects.filter(user = self.user, name = payload['name']).exists()
+
+        self.assertTrue(res)
+
+    def test_create_tag_with_bad_information(self):
+        """test that we cant create tag with invalid string such as blank space"""
+        payload = {'name' : ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
